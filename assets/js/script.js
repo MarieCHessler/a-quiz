ae5d3992aac945face868303a54d7abf0d35aff1
MarieCHessler <marie@hessler.se>
2021-11-26 20:51:41 +0000

    Add constant references to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
+/* Constant references */
+const question = document.getElementById('question');
+const answerAlternatives = document.getElementsByClassName('answer-alternative')
+
 fetch ('https://opentdb.com/api.php?amount=50&category=27&type=multiple')
