04553dff48f6c4226498dec763bca2174edd358d
MarieCHessler <marie@hessler.se>
2021-11-28 11:12:06 +0000

    Add quiz variables for the present question, the question counter, the present score and the dummy questions from json file, to JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
-// Constant references
+// Constant references Quiz
 const question = document.getElementById("question");
 const answerAlternatives = Array.from(document.getElementsByClassName("answer-alternative"));
-console.log(answerAlternatives)
+
+// Variables Quiz
+let presentQuestion = {}
+let score = 0;
+let questionCounter = 0;
+
+let questions = [
+    {
+        "quiz-question": "What is the main focus of this project?",
+        "answer-alternative1": "CSS",
+        "answer-alternative2": "JavaScript",
+        "answer-alternative3": "HTML",
+        "answer": "2"
+    },
+
+    {
+        "quiz-question": "What is this project about?",
+        "answer-alternative1": "Creating a game",
+        "answer-alternative2": "Creating an e-book",
+        "answer-alternative3": "Creating a web shop",
+        "answer": "1"
+    },
+
+    {
+        "quiz-question": "When is the deadline?",
+        "answer-alternative1": "December 24",
+        "answer-alternative2": "January 1",
+        "answer-alternative3": "December 11",
+        "answer": "3"
+    }
+]
 
 //fetch ('https://opentdb.com/api.php?amount=50&category=27&type=multiple')
