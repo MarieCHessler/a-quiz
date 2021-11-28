b5e3fc8a03f9815a681b9509d69d65bbfcf2ce47
MarieCHessler <marie@hessler.se>
2021-11-28 10:33:38 +0000

    Adjusting script to run console log and test functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-/* Constant references */
-const question = document.getElementById('question');
-const answerAlternatives = document.getElementsByClassName('answer-alternative')
+// Constant references
+const question = document.getElementById("question");
+const answerAlternatives = document.getElementsByClassName("answer-alternative");
+console.log(answerAlternatives);
 
-fetch ('https://opentdb.com/api.php?amount=50&category=27&type=multiple')
+//fetch ('https://opentdb.com/api.php?amount=50&category=27&type=multiple')
