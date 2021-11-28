9589e556ff6e27d071f343231bf8e36501e5f6f3
MarieCHessler <marie@hessler.se>
2021-11-28 10:39:21 +0000

    Change script to create Array instead of HTML Collection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Constant references
 const question = document.getElementById("question");
-const answerAlternatives = document.getElementsByClassName("answer-alternative");
-console.log(answerAlternatives);
+const answerAlternatives = Array.from(document.getElementsByClassName("answer-alternative"));
+console.log(answerAlternatives)
 
 //fetch ('https://opentdb.com/api.php?amount=50&category=27&type=multiple')
