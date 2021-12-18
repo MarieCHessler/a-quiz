934ec8a3633c6e4ca34cf1cd118443a80bd5ad70
MarieCHessler <marie@hessler.se>
2021-12-18 15:41:17 +0000

    Change double quotes to single, change indentation, add changeAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,16 @@
 let question;
 let choices;
 let questionCounterDiv;
-let choiceButtons;
 
 // Event Listener for whole window
 window.addEventListener('DOMContentLoaded', (event) => {
   // Constant references for Quiz
-  question = document.getElementById("question");
-  choices = Array.from(document.getElementsByClassName("choice"));
-  questionCounterDiv = document.getElementById("question-counter");
+  question = document.getElementById('question');
+  choices = Array.from(document.getElementsByClassName('choice'));
+  questionCounterDiv = document.getElementById('question-counter');
 
-  choiceButtons = document.getElementsByClassName ("choice");
-    for (btn in choiceButtons) {
+    for (btn in choices) {
+      checkAnswer();
       console.log(btn);
     }
 
@@ -20,21 +19,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 // Restart Quiz
 startQuiz = () => {
-    questionCounter = 0;
-    score = 0;
-    remainingQuestions = [...questions];
-    getNextQuestion();
+  questionCounter = 0;
+  score = 0;
+  remainingQuestions = [...questions];
+  getNextQuestion();
 };
 
 getNextQuestion = () => {
-    questionCounter++;
-    let questionIndex = Math.floor(Math.random() * remainingQuestions.length);
-    presentQuestion = remainingQuestions[questionIndex];
-    question.innerText = presentQuestion ["question"];
-    for (let i = 0; i<3; i++){
-      choices[i].innerText = presentQuestion[`choice${i+1}`];
-    }
-    questionCounterDiv.innerText = `${questionCounter}/${questions.length}`;
+  questionCounter++;
+  let questionIndex = Math.floor(Math.random() * remainingQuestions.length);
+  presentQuestion = remainingQuestions[questionIndex];
+  question.innerText = presentQuestion['question'];
+  for (let i = 0; i<3; i++){
+    choices[i].innerText = presentQuestion[`choice${i+1}`];
+  }
+  questionCounterDiv.innerText = `${questionCounter}/${questions.length}`;
 };
+
+checkAnswer = (e) => {
+  if(correctAnswer == userAnswer) {
+    score++;
+    getNextQuestion();
+  } else {
+    getNextQuestion();
+  }
+}
 
 //fetch ('https://opentdb.com/api.php?amount=50&category=27&type=multiple')
