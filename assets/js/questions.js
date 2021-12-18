d856215c16136189ea0e9aa5e0e0e8894cc3d42f
MarieCHessler <marie@hessler.se>
2021-12-18 21:35:52 +0000

    Add new, and more, dummy questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,6 @@
 //Constants and variables
 const pointsForCorrectAnswer = 1;
-const numberOfQuestions = 5;
+const numberOfQuestions = 10;
 let presentQuestion = {};
 let acceptingAnswers = false;
 let questionCounter = 0;
@@ -9,50 +9,170 @@ let remainingQuestions = [];
 
 let questions = [
     {
-        "question": "What is the main focus of this project?",
-        "choice1": "CSS",
-        "choice2": "JavaScript",
-        "choice3": "HTML",
-        "correctAnswer": "2"
-    },
-
-    {
-        "question": "What is this project about?",
-        "choice1": "Creating a game",
-        "choice2": "Creating an e-book",
-        "choice3": "Creating a web shop",
+        "question": "Dummy 1.0?",
+        "choice1": "Answer 1.0",
+        "choice2": "Answer 1.1",
+        "choice3": "Answer 1.2",
         "correctAnswer": "1"
     },
 
     {
-        "question": "When is the deadline?",
-        "choice1": "December 24",
-        "choice2": "January 1",
-        "choice3": "December 11",
+        "question": "Dummy 1.1?",
+        "choice1": "Answer 1.0",
+        "choice2": "Answer 1.1",
+        "choice3": "Answer 1.2",
+        "correctAnswer": "2"
+    },
+
+    {
+        "question": "Dummy 1.2?",
+        "choice1": "Answer 1.0",
+        "choice2": "Answer 1.1",
+        "choice3": "Answer 1.2",
         "correctAnswer": "3"
     },
 
     {
-        "question": "What theme does the quiz have",
-        "choice1": "Music",
-        "choice2": "Disney",
-        "choice3": "Animals",
-        "correctAnswer": "3"
-    },
-
-    {
-        "question": "How many answers do you have to choose from?",
-        "choice1": "Three",
-        "choice2": "Two",
-        "choice3": "One",
+        "question": "Dummy 1.3?",
+        "choice1": "Answer 1.3",
+        "choice2": "Answer 1.4",
+        "choice3": "Answer 1.5",
         "correctAnswer": "1"
     },
 
     {
-        "question": "How difficult is JavaScript?",
-        "choice1": "Not at all",
-        "choice2": "Very",
-        "choice3": "A little",
+        "question": "Dummy 1.4?",
+        "choice1": "Answer 1.3",
+        "choice2": "Answer 1.4",
+        "choice3": "Answer 1.5",
         "correctAnswer": "2"
-    }
+    },
+
+    {
+        "question": "Dummy 1.5?",
+        "choice1": "Answer 1.3",
+        "choice2": "Answer 1.4",
+        "choice3": "Answer 1.5",
+        "correctAnswer": "3"
+    },
+
+    {
+        "question": "Dummy 1.6?",
+        "choice1": "Answer 1.6",
+        "choice2": "Answer 1.7",
+        "choice3": "Answer 1.8",
+        "correctAnswer": "1"
+    },
+
+    {
+        "question": "Dummy 1.7?",
+        "choice1": "Answer 1.6",
+        "choice2": "Answer 1.7",
+        "choice3": "Answer 1.8",
+        "correctAnswer": "2"
+    },
+
+    {
+        "question": "Dummy 1.8?",
+        "choice1": "Answer 1.6",
+        "choice2": "Answer 1.7",
+        "choice3": "Answer 1.8",
+        "correctAnswer": "3"
+    },
+
+    {
+        "question": "Dummy 1.9?",
+        "choice1": "Answer 1.9",
+        "choice2": "Answer 2.0",
+        "choice3": "Answer 2.1",
+        "correctAnswer": "1"
+    },
+
+    {
+        "question": "Dummy 2.0?",
+        "choice1": "Answer 1.9",
+        "choice2": "Answer 2.0",
+        "choice3": "Answer 2.1",
+        "correctAnswer": "2"
+    },
+
+    {
+        "question": "Dummy 2.1?",
+        "choice1": "Answer 1.9",
+        "choice2": "Answer 2.0",
+        "choice3": "Answer 2.1",
+        "correctAnswer": "3"
+    },
+
+    {
+        "question": "Dummy 2.2?",
+        "choice1": "Answer 2.2",
+        "choice2": "Answer 2.3",
+        "choice3": "Answer 2.4",
+        "correctAnswer": "1"
+    },
+
+    {
+        "question": "Dummy 2.3?",
+        "choice1": "Answer 2.2",
+        "choice2": "Answer 2.3",
+        "choice3": "Answer 2.4",
+        "correctAnswer": "2"
+    },
+
+    {
+        "question": "Dummy 2.4?",
+        "choice1": "Answer 2.2",
+        "choice2": "Answer 2.3",
+        "choice3": "Answer 2.4",
+        "correctAnswer": "3"
+    },
+
+    {
+        "question": "Dummy 2.5?",
+        "choice1": "Answer 2.5",
+        "choice2": "Answer 2.6",
+        "choice3": "Answer 2.7",
+        "correctAnswer": "1"
+    },
+
+    {
+        "question": "Dummy 2.6?",
+        "choice1": "Answer 2.5",
+        "choice2": "Answer 2.6",
+        "choice3": "Answer 2.7",
+        "correctAnswer": "2"
+    },
+
+    {
+        "question": "Dummy 2.7?",
+        "choice1": "Answer 2.5",
+        "choice2": "Answer 2.6",
+        "choice3": "Answer 2.7",
+        "correctAnswer": "3"
+    },
+
+    {
+        "question": "Dummy 2.8?",
+        "choice1": "Answer 2.8",
+        "choice2": "Answer 2.9",
+        "choice3": "Answer 3.0",
+        "correctAnswer": "1"
+    },
+
+    {
+        "question": "Dummy 2.9?",
+        "choice1": "Answer 2.8",
+        "choice2": "Answer 2.9",
+        "choice3": "Answer 3.0",
+        "correctAnswer": "2"
+    },
+
+    {
+        "question": "Dummy 3.0?",
+        "choice1": "Answer 2.8",
+        "choice2": "Answer 2.9",
+        "choice3": "Answer 3.0",
+        "correctAnswer": "3"
+    },
 ];
